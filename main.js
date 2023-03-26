c1796ccc83caf444062612f0d8fe0c6dfc5f2405
Mounica-93 <78643750+Mounica-93@users.noreply.github.com>
2023-03-26 12:38:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+objects=[];
+video="";
+status="";
+
+function preload(){
+    video=createVideo('video.mp4');
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video.hide();
+}
+
+function start(){
+    objectdetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting objects";
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function draw(){
+    image(video,0,0,480,380);
+
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML="Status- Object detected";
+            document.getElementById("no_of_objs").innerHTML="Number of objects detected are- "+objects.length;
+            fill("green");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+            nofill();
+            stroke("green");
+            rect(objects[i].x,objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results){
+  if(error){
+    console.log(error)
+  }
+  console.log(results);
+  objects=results;
+}
+
